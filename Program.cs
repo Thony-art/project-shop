8831651fb2f03e0f19c5fe17a48895c262674188
Thony <thony.lituba@edouardgand.fr>
2026-03-11 16:49:25 +0100

    Partie trois la vrai

--- Program.cs
+++ Program.cs
@@ -14,8 +14,11 @@ class Program
         Console.WriteLine($"Customer: {c1.GetFirstname()} {c1.GetLastname()}, Email: {c1.GetEmail()}");
         Console.WriteLine($"Order Number: {o1.GetOrderNumber()}, Total Amount: {o1.GetTotalAmount()}, Is Paid: {o1.IsPaid()}");
         Item item1 = new Item("Phone", 800, 2);
-        Console.WriteLine($"Item: {item1.Name}, Price: {item1.Price}, Quantity: {item1.Quantity}");  
+        Console.WriteLine($"Item: {item1.Name}, Price: {item1.Price}, Quantity: {item1.Quantity}"); 
+        Supplier supplier1 = new Supplier("Tech Supplies Inc.", "New York", "0102030405"); 
+        Console.WriteLine($"Supplier: {supplier1.Name}, City: {supplier1.City}, Phone: {supplier1.Phone}");
     }
+
 
 }
  
